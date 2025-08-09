dfa0f17eb0d4f2b309331679ffe9c5d7fdabbe7c
lakshitha <lakshitham022@gmail.com>
2025-08-09 13:08:06 +0530

    Update size-of-array.c

--- arrays/size-of-array.c
+++ arrays/size-of-array.c
@@ -6,13 +6,13 @@ int main() {
     scanf("%d",&n);    // Gets input from user
     printf("Enter the elements: \n");
     for(i=0;i<n;i++){      // loop to store the elements in the array
-     scanf("%d",&arr[i]);   // looping variable i is initialised from 0 to <n because the last index (i.e. n) stores the NULL value to indicate the end of the array
+        scanf("%d",&arr[i]);   // looping variable i is initialised from 0 to <n because the last index (i.e. n) stores the NULL value to indicate the end of the array
     }
     printf("\n");
     printf("The array is: \n");
     for(i=0;i<n;i++){       // loop to print the array elements
-     arr[n]=arr[n-1];    // for every element in index n, the loop runs for the previous element in index n-1, till it reaches index 0
-     printf("%d ",arr[i]);  // here arr[i] accesses the array element stored at index i
+        arr[n]=arr[n-1];    // for every element in index n, the loop runs for the previous element in index n-1, till it reaches index 0
+        printf("%d ",arr[i]);  // here arr[i] accesses the array element stored at index i
     }
     printf("\n");
     printf("The size of array is: \n");
@@ -29,7 +29,7 @@ int main() {
     int i;
     printf("the array is: ");
     for(i=0;i<5;i++){       // looping statement to print the array elements
-      printf("%d ",arr[i]);
+        printf("%d ",arr[i]);
     }
     printf("\n");
     printf("The size of array is: ");
